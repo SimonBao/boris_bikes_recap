d1c00b1eba7763dd22e9926519789a407d08fdeb
Simon Bao <Simonbao.sb@gmail.com>
2018-03-22 11:40:54 +0000

    Test - expects docking station to have 'release_bike' method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,5 +4,9 @@ describe DockingStation do
   it 'initialises with an empty dock' do 
     expect(subject.dock).to be_empty
   end
+
+  describe '#release_bike' do 
+    it { is_expected.to respond_to :release_bike }
+  end
   
 end
