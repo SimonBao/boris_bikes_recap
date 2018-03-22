c4cf40112c2ddd5ba7fb75cf633aaa8cc7a1ea66
Simon Bao <Simonbao.sb@gmail.com>
2018-03-22 11:46:51 +0000

    Test Pass - 'uninitialized constant Bike'

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,10 @@
 require 'bike'
 
 describe Bike do
+  it { is_expected.to respond_to :working?}
+  describe '#working?' do 
+    it 'returns true' do 
+      expect(subject.working?).to be true 
+    end
+  end
 end
